a2aba4ce098e163dee1e21cae7cedf89dc25515b
Santiago Lacayo (NYU) <santiagolacayonyu@10-20-120-166.dynapool.wireless.nyu.edu>
2025-09-25 09:33:55 -0400

    classwork

--- week4/com-lib/src/components/Dropdown.js
+++ week4/com-lib/src/components/Dropdown.js
@@ -1,24 +1,44 @@
+// whenever sometthing needs to change bby an interaction always use useState
 import {useState} from 'react'
+// classnames library to merge our classes
 import cx from 'classnames'
+import {GoChevronDown} from 'react-icons/go'
 
 const Dropdown = (props) => {
-    const {options} = props
+    const {options, onChange, value} = props
     // local state
-    const [isOpen, setIsOpen] = useState(true) //false means the dropdown will be closed at start
+    const [isOpen, setIsOpen] = useState(false) //false means the dropdown will be closed at start
+   
+    // simple event
+    const handleClick = () => {
+        setIsOpen(!isOpen)
+    }
+
+    const handleOptionClick = (option) => {
+        setIsOpen(false)
+        // need some other function defined by the parent component passed in as a prop to call here
+        onChange(option)
+    }
+
     const renderedOptions = options.map((opt, index) => {
         return ( 
-        <div key={index} className="hover:bg-sky-100 rounded cursor-pointer p-1">
-            {opt.label}
+        <div 
+        onClick={() => handleOptionClick(opt)} 
+        key={index} 
+        className="hover:bg-sky-100 rounded cursor-pointer p-1">
+        {opt.label}
         </div>
-        )
+         )
     })
 
     return (
         <div className="w-48 relative">
-            <Panel className="flex justify-between items-center cursor-pointer">
-                {/* conditional text coming soon */}
-                Select...
-                
+            <Panel 
+            onClick = {handleClick}
+            className="flex justify-between items-center cursor-pointer">
+                {/* great use of a ternary */}
+                {/* ? if it has a value do something if not leave it like that {value?.value} */}
+                {value ? value.label : 'Select...'} <GoChevronDown />
             </Panel>
             {isOpen && <Panel className="absolute top-full">{renderedOptions}</Panel>}
         </div>
@@ -28,16 +48,17 @@ const Dropdown = (props) => {
 
 
 
-
 // this is how you make a function
 const Panel = (props) => {
     const {className, children, ...rest} = props
     const finalClassNames = cx(
-        props.className, 
+        className, 
         'border rounded p-3 shadow bg-white w-full')
-    return <div className={finalClassNames} >
+    return (
+        <div {...rest} className={finalClassNames} >
         {children}
         </div>
+    )
 }
 
 export {Panel}
--- week4/com-lib/src/pages/DropdownPage.js
+++ week4/com-lib/src/pages/DropdownPage.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import {useState} from 'react'
 import Dropdown from '../components/Dropdown'
 
 const OPTIONS = [ // array of options objects
@@ -6,11 +6,55 @@ const OPTIONS = [ // array of options objects
     {label: 'Green', value: 'green'}, 
     {label: 'Blue', value: 'blue'}, 
 ]
+
+const COLOR_MAP = {
+    red: 'bg-red-500',
+    green: 'bg-green-400',
+    blue: 'bg-blue-500',
+}
+
+// example usage of COLOR_MAP
+// className={COLOR_MAP[value.value]
+// template literals
+// const colorClass = `bg-${value?.value}-500`
+
+const DATA_TO_FILTER = [
+    {id: 1, name: 'Santi', team: 'red'},
+    {id: 1, name: 'Emilio', team: 'green'},
+    {id: 1, name: 'Nicolas', team: 'blue'},
+    {id: 1, name: 'Arturo', team: 'red'},
+    {id: 1, name: 'Elias', team: 'blue'},
+]
+
 const DropdownPage = () => {
+    // this piece of state is where we receive our dropdown selected value, we keep track of it in the parent component so that it, 
+    // and all children have access to this piece of states value and can update and render
+    const [value, setValue] = useState(null)
+
+    let filteredData = DATA_TO_FILTER
+
+    // if the user selected an option from the dropdown, find the value key, if the value exists
+    if (value?.value) {
+        // filter our array by the of seleced option
+        filteredData = DATA_TO_FILTER.filter((student) => student.team === value.value)
+    }
+
+    const handleChange = (option) => {
+        setValue(option)
+    }
+
     return (
       <div>
-        <h1>Dropdown Page</h1>
-        <Dropdown options={OPTIONS}/>
+        <h1 className={COLOR_MAP[value?.value]}>
+            Dropdown Page with user selected value of: {value?.label}
+        </h1>
+        <Dropdown options={OPTIONS} onChange={handleChange} value={value}/>
+        <h2 className={COLOR_MAP[value.value]}>Students from {value?.label}:</h2>
+        {
+            filteredData.map((student) =>{
+                return <p key={student.id}>{student.name}</p>
+            })
+        }
       </div>
     )
   }
